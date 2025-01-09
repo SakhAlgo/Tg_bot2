b9185d0fc4a009cd7d60244f2ef16935cd874437
SakhAlgo <sakhalgo@gmail.com>
2025-01-10 08:23:13 +1100

    add bot, dp

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from config_data.config import load_config
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
 
-conf = load_config('.env')
+config = load_config('.env')
 
-bot_token = conf.tg_bot.token
-print(bot_token)
+bot = Bot(token=config.tg_bot.token)
+dp = Dispatcher()
+
+some_var_1 = 1
+some_var_2 = 'Some text'
+
+dp.workflow_data.update({'my_int_var': some_var_1, 'my_text_var': some_var_2})
+
+
+@dp.message(Command(commands='start'))
+async def process_start_command(message: Message, my_int_var, my_text_var):
+    await message.answer(text=str(my_int_var))
+    await message.answer(text=my_text_var)
+    
+if __name__ == '__main__':
+    dp.run_polling(bot)
